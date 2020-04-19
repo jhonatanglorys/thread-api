f618d490276aef4c5a45dafc80885c7bb3837e89
jhonatanglorys <jhonatanglorys@gmail.com>
2020-04-18 21:35:01 -0500

    punto 1

--- punto1.c
+++ punto1.c
@@ -4,7 +4,13 @@
 #include <time.h>
 #include <pthread.h>
 
-long long int factorial (void* arg);
+struct num_fact
+{
+  int num;
+};
+
+
+void* factorial (void* arg);
 
 int main (int argc, char *argv[]) {
     srand(time(0)); 
@@ -14,10 +20,12 @@ int main (int argc, char *argv[]) {
         int* fact_dos;
       pthread_t id_hilo_uno;
       pthread_t id_hilo_dos;  
-      int param_uno = atoi(argv[1]);
-      int param_dos = atoi(argv[2]);
-      pthread_create(&id_hilo_uno,NULL,(void *)factorial,&param_uno);
-      pthread_create(&id_hilo_dos,NULL,(void *)factorial,&param_dos);
+      struct num_fact param_uno;
+      struct num_fact param_dos;
+      param_uno.num = atoi(argv[1]);
+      param_dos.num = atoi(argv[2]);
+      pthread_create(&id_hilo_uno,NULL,&factorial,&param_uno);
+      pthread_create(&id_hilo_dos,NULL,&factorial,&param_dos);
       pthread_join(id_hilo_uno,(void *)&fact_uno);
       pthread_join(id_hilo_dos,(void *)&fact_dos);
     printf ("El factorial de %d es %d\n", param_uno, *fact_uno);
@@ -30,13 +38,13 @@ int main (int argc, char *argv[]) {
 }
 
 
-long long int factorial(void* arg){
-    int fact = *((int*)arg);
-    long long int resultado= 1;
+void* factorial(void* parameters){
+    struct num_fact* fact = (struct num_fact*) parameters;
+    long resultado= 1;
     int num;
-    for (num= 2; num<=fact; num++) {
+    for (num= 2; num<=fact->num; num++) {
         resultado= resultado*num;
-        printf ("Factorial de %d, resultado parcial %lld\n", fact, resultado);
+        printf ("Factorial de %d, resultado parcial %lld\n", fact->num, resultado);
         sleep (random()%3);
     }
     return resultado;
